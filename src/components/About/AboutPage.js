da066c41e53f89f52c457d96231e6d36d12ae888
urkqsrk <urkqsrk@abv.bg>
2016-12-19 21:13:26 +0200

    бб

--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -3,7 +3,7 @@ import React, {Components} from 'react';
 export default class AboutPage extends Component {
     render() {
         return (
-            <div>About Page GitHub </div>
+            <div>About Page</div>
         );
     }
 }
